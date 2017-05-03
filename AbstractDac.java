f607a6cdb12316843ee33958d8221021900ccfe0
Osamu Hashimoto <osamu@hashimoto-net.jp>
2017-05-03 09:19:59 +0900

    実用に耐えるよう、いくつか修正。

--- AbstractDac.java
+++ AbstractDac.java
@@ -1,5 +1,5 @@
 /**
- * AbstractDac.java ver. 0.7
+ * AbstractDac.java ver. 1.0
  */
 
 package hashimotonet.sql;
@@ -57,16 +57,28 @@ public abstract class AbstractDac {
     }
 
     /**
+     * 結果セットのカーソル位置の
      *
      * @param bean
      * @param rs
      * @return
      * @throws SQLException
+     * @throws IllegalAccessException
      */
-    public Object setData(Object bean, ResultSet rs) throws SQLException {
+    public Object setData(Object bean, ResultSet rs)
+            throws SQLException,
+            IllegalAccessException,
+            InvocationTargetException {
+        // 結果セットよりメタデータ取得。
         ResultSetMetaData rsMeta = rs.getMetaData();
+
+        // 結果セットのカラム数を取得。
         int count = rsMeta.getColumnCount();
+
+        // カラム名のString配列
         String columnNames[] = new String[count];
+
+        // カラムの型
         int columnTypes[] = new int[count];
 
         System.out.println("count = " + count);
@@ -74,38 +86,51 @@ public abstract class AbstractDac {
         // カラム名取得。
         for (int i = 1; i <= columnNames.length; i++) {
             System.out.println("i = " + i);
+
+            // カラム名を取得。
             columnNames[i - 1] = rsMeta.getColumnName(i);
             System.out.println(columnNames[i - 1]);
+
+            // カラムの型を取得。
             columnTypes[i - 1] = rsMeta.getColumnType(i);
             System.out.println(columnTypes[i - 1]);
         }
 
         // Beanのセッターメソッド取得。
-        Class beanClass = bean.getClass();
-        Method methods[] = beanClass.getDeclaredMethods();
-        //        String setterNames[] = getSetterNames(bean);
+        Class beanClass = bean.getClass(); // クラスの取得
+        Method methods[] = beanClass.getDeclaredMethods(); // クラスの宣言メソッドを取得
 
-        //        while (rs.next()) {
+        // ループカウンタ
         int i = 0;
+
+        // カラム名数分だけループする。
         for (String columnName : columnNames) {
+
+            // メソッド名分だけループする。
             for (Method method : methods) {
+
+                // メソッド名取得。
                 String methodName = method.getName();
-                if (columnName.equalsIgnoreCase(methodName.substring(3))
-                        && methodName.startsWith("set")) {
+
+                // カラム名よりアンダースコアを削除した名称とメソッド名が紐づくか
+                if (deleteUnderscore(columnName).equalsIgnoreCase(methodName.substring(3))
+                        && methodName.startsWith("set")) { // カラム名と同一でsetterメソッドの場合
                     // beanにプロパティセット
-                    if (Types.CHAR == columnTypes[i]) {
+                    if (Types.CHAR == columnTypes[i]) { // カラムがCHAR型である
                         // String型をセット。
+                        invoke(rs.getString(columnName), bean, method);
 
-                    } else if (Types.INTEGER == columnTypes[i]) {
+                    } else if (Types.INTEGER == columnTypes[i]) { //　カラムがINT型である
                         // Int型をセット。
-
+                        invoke(rs.getInt(columnName), bean, method);
                     }
                 }
             }
+            // カウンタをインクリメント。
             i++;
         }
-        //        }
 
+        // プロパティセットされたBeanを返却する。
         return bean;
     }
 
@@ -119,7 +144,11 @@ public abstract class AbstractDac {
      */
     public Object invoke(int param, Object bean, Method method)
             throws InvocationTargetException, IllegalAccessException {
+
+        // Beanのセッターメソッドにパラメータをセット。
         method.invoke(bean, param);
+
+        // パラメータのセットされたBeanを返却する。
         return bean;
     }
 
@@ -135,14 +164,42 @@ public abstract class AbstractDac {
      */
     public Object invoke(String param, Object bean, Method method)
             throws InvocationTargetException, IllegalAccessException {
+
+        // Beanのセッターメソッドにパラメータをセット。
         method.invoke(bean, param);
+
+        // パラメータのセットされたBeanを返却する。
         return bean;
     }
 
     /**
+     * カラム名よりアンダースコアを削除する。
+     *
+     * @param columnName
+     * @return
+     */
+    protected String deleteUnderscore(String columnName) {
+        StringBuilder column = new StringBuilder(); // 文字列バッファ
+
+        // カラム名をアンダースコアをデリミタにして分解。
+        String split[] = columnName.split("_");
+
+        // デリミタで分解された文字列を結合。
+        for (String part : split) {
+            column.append(part);
+        }
+
+        // アンダースコアを削除したカラム名を返却する。
+        return column.toString();
+
+    }
+
+    /**
      * デストラクタ。
+     * GCによる当クラスの具象クラスアンロード時にコールされる。
      */
     public void finalize() throws SQLException {
+        //　JDBC接続マネージャをクローズ。
         conn.close();
     }
 
